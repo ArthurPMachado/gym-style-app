5c90c8bff3b9fc0320f7b7937a113a96d92dc3d7
Arthur Machado <arthur.machado@iclubs.tech>
2023-11-09 00:00:25 -0300

    create register user route

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,19 @@
 import fastify from 'fastify'
+import { registerBodySchema } from './schemas/register-body-schema'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
