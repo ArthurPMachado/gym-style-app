bf67bfcb7125c8e2099bedc72fe3dae34bc5001b
Arthur Machado <arthur.machado@iclubs.tech>
2023-11-14 16:25:55 -0300

    apply dependency inversion

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,12 +1,16 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { registerBodySchema } from '@/schemas/register-body-schema'
-import { registerUserCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUserCase({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUserCase = new RegisterUseCase(usersRepository)
+
+    await registerUserCase.execute({
       name,
       email,
       password,
