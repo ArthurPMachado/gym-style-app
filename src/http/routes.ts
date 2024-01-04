6a75429d19acff440b3b5871f311f5fb859dd2cc
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-04 18:52:17 -0300

    feat: use authentication in all routes

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register-controller'
 import { authenticate } from './controllers/authenticate'
-// import { profile } from './controllers/profile'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  // app.get('/me', profile)
+  app.get('/me', profile)
 }
