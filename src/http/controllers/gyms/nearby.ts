f5948fdc00b5aec2133f0ccea8600417afcad372
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-11 00:25:54 -0300

    feat: create search and nearby gyms controllers

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,18 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { nearbyGymsQuerySchema } from '@/schemas/nearby-gyms-query-schema'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,18 @@
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { searchGymsQuerySchema } from '@/schemas/search-gyms-query-schema'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const { query, page } = searchGymsQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
